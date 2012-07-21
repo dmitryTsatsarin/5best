3a8a0bfd7bbb273f56535633454bc3052cb2f6a9
Michael Borisov <niro@tut.by>
2012-07-21 16:35:44 +0300

    Requirements, git ignore. Base main model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+
+admin.site.register(Group)
+
+class Proposal(models.Model):
+    name = models.CharField(max_length=255, unique = True)
+
+admin.site.register(Proposal)
+
+class UserProposal(models.Model):
+    proposal = models.ForeignKey(Proposal)
+    placenumber = models.PositiveIntegerField()
+
+admin.site.register(UserProposal)
+
+class UserGroup(models.Model):
+    user = models.ForeignKey(User)
+    group = models.ForeignKey(Group)
+    proposals = models.ManyToManyField(UserProposal)
+
+admin.site.register(UserGroup)
